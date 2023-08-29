a2b28fe65a25eb71c02c6172182cfbe6ee238715
chrisdkolloff <chrisdkolloff@gmail.com>
2023-08-29 17:01:59 +0200

    typo in package name

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dynamic_utils',
+    name='dynamics_utils',
     author='chrisdkolloff',
     author_email='chrisdkolloff@gmail.com',
     description='Utils for dealing with dynamic data (MSM, NMR, etc.)',
-    url='https://github.com/chrisdkolloff/dynamic_utils.git',
+    url='https://github.com/chrisdkolloff/dynamics_utils.git',
     version='0.0',
-    packages=['dynamic_utils'],
+    packages=['dynamics_utils'],
     install_requires=[
         'numpy', 'torch'
     ],
-    py_modules=['dynamic_utils'],
+    py_modules=['dynamics_utils'],
 )
